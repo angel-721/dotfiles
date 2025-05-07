12d4f887aba51bca09b8d7064c24bfa0f185ded3
angel-721 <angelvelasquez2002@gmail.com>
2025-05-07 10:18:39 -0400

    I listen to way too much of The Story So Far

--- new/nvim_1/init.lua
+++ new/nvim_1/init.lua
@@ -18,17 +18,6 @@ require("config.lazy")
 local api = require "nvim-tree.api"
 vim.keymap.set('n', 'tr', api.tree.open, {})
 vim.keymap.set('n', 'ty', api.tree.close, {})
-local function setup_lsp_diags()
-  vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-    vim.lsp.diagnostic.on_publish_diagnostics,
-    {
-      virtual_text = false,
-      signs = true,
-      update_in_insert = false,
-      underline = true,
-    }
-  )
-end
 
 vim.diagnostic.config({
   virtual_text = true, -- Shows diagnostics inline with code
@@ -45,6 +34,7 @@ vim.lsp.enable('cssls')
 vim.lsp.enable('htmx')
 vim.lsp.enable('html')
 vim.lsp.enable('basedpyright')
+vim.lsp.enable('svelte')
 
 -- Load after loading plugin
 vim.cmd.colorscheme "catppuccin-mocha"
