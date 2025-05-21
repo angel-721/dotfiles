37b6e6916630f2f0f2a994b8b438eac9de1bd991
angel-721 <angelvelasquez2002@gmail.com>
2025-05-21 12:15:16 -0400

    Update formatters

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,7 @@
 vim.o.number = true
 vim.o.signcolumn = 'yes'
-vim.o.tabstop = 4
-vim.o.shiftwidth = 4
+-- vim.o.tabstop = 2
+-- vim.o.shiftwidth = 2
 vim.o.updatetime = 300
 vim.o.termguicolors = true
 vim.g.loaded_netrw = 1
--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,23 +1,52 @@
 return {
   'stevearc/conform.nvim',
   opts = {
-  formatters_by_ft = {
-    python = { "ruff_organize_imports", "ruff_format", "ruff_fix" },
-    ---javascript = { { "prettierd", "prettier" } },
-    css = {  "prettierd", "prettier"  },
-    html = {  "prettierd", "prettier"  },
-    astro = {  "prettierd", "prettier"  },
-    nix = {"alejandra "},
-    cpp = {"clang-format"} ,
-    zig = {"zigfmt"},
-    rust = {"rustfmt"},
-    ["*"] = { "codespell" },
-  },
+    formatters_by_ft = {
+      python = { "ruff_organize_imports", "ruff_format", "ruff_fix" },
+      javascript = { "prettierd" },
+      typescript = { "prettierd" },
+      typescriptreact = { "prettierd" }, -- Add TSX support
+      javascriptreact = { "prettierd" }, -- Add JSX support
+      css = { "prettierd" },
+      html = { "prettierd" },
+      astro = { "prettierd" },
+      nix = { "alejandra" }, -- Fixed the space issue
+      cpp = { "clang-format" },
+      zig = { "zigfmt" },
+      rust = { "rustfmt" },
+      ["*"] = { "codespell" },
+    },
 
-  format_on_save = {
-    lsp_fallback = true,
-    timeout_ms = 500,
-  },
-	}
-	
+    format_on_save = {
+      lsp_fallback = true,
+      timeout_ms = 500,
+    },
+
+    formatters = {
+      prettierd = {
+        options = {
+          tabWidth = 2,
+          useTabs = false,
+          semi = true,
+          singleQuote = false,
+          trailingComma = "es5",
+          bracketSpacing = true,
+          jsxSingleQuote = false,
+          jsxBracketSameLine = false,
+        }
+      },
+      prettier = {
+        options = {
+          tabWidth = 2,
+          useTabs = false,
+          semi = true,
+          singleQuote = false,
+          trailingComma = "es5",
+          bracketSpacing = true,
+          jsxSingleQuote = false,
+          jsxBracketSameLine = false,
+        }
+      },
+    },
+  }
 }
